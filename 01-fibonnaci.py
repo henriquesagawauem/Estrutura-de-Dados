72f3c64534cc4cbea44139e862e344216f245dee
henriquesagawauem <ra152725@uem.br>
2026-08-10 21:07:29 -0300

    atualização atividade

--- 01-fibonnaci.py
+++ 01-fibonnaci.py
@@ -8,9 +8,12 @@ for i in range(n - 1):
 
     soma = soma + b
     if i == n - 2:
-        print(str(a) + " + " + str(b) + " =", + soma)
+        print(str(a))
+        print(str(b))
     else:
-        print(str(a) + " + ", end="")
+        print(str(a))
     temp = b
     b = a + b
     a = temp
+
+print("soma = " + str(soma))
